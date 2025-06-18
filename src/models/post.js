3e1388cd5a988aa7e92bcf7b313e526c8bdfd09b
LSantana32 <lucasnahuel.santana@estudiantes.unahur.edu.ar>
2025-06-17 21:10:27 -0300

    definicion de modelo Post

---
+++ src/models/post.js
@@ -0,0 +1,39 @@
+const { mongoose } = require("../config/db");
+const { Schema } = require("mongoose");
+
+const postSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: [true, 'El userId es obligatorio'],
+        },
+        content: {
+            type: Schema.Types.String,
+            required: [true, 'El contenido es obligatorio'],
+            minlength: [5, 'El contenido debe tener al menos 5 carácter'],
+            maxlength: [500, 'El contenido no puede exceder los 500 caracteres'],
+        },
+        fecha: {
+            type: Schema.Types.Date,
+            default: Date.now,
+        },
+        imagenes: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Archive',
+        }]
+    },
+    {
+        collection: "posts",
+    }
+);
+
+postSchema.set("toJSON", {
+    transform: (_, ret) => {
+        delete ret.__v;
+        delete ret._id;
+    },
+});
+
+const Post = mongoose.model("Post", postSchema);
+module.exports = Post;
